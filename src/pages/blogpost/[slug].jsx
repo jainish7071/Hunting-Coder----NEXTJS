da6de07857b3de4e5f91ffb35f5e603fd389167b
Jainish Kunjadiya <jainishkunjadiya2712@gmail.com>
2023-04-09 11:58:31 +0530

    added dynamic paths for static web creation

--- src/pages/blogpost/[slug].jsx
+++ src/pages/blogpost/[slug].jsx
@@ -41,8 +41,12 @@ const Slug = (props) => {
 
 // For Generating HTML On Static Side and send to client when he requests
 export async function getStaticPaths() {
+  let allBlog = await fs.promises.readdir("/blogdata");
+  allBlog = allBlog.map((item) => {
+    return { params: { slug: item.split(".")[0] } };
+  });
   return {
-    paths: [{ params: { slug: "how-to-learn-flask" } }, { params: { slug: "how-to-learn-javascript" } }, { params: { slug: "how-to-learn-nextjs" } }],
+    paths: allBlog,
     fallback: false, // can also be true or 'blocking'
   };
 }
